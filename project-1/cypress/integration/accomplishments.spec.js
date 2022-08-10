a20e095a5eb899da72f52d72981fe6375bf2fcb5
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-08-10 16:06:35 +0500

    added test for async operation of accmp submittion

--- project-1/cypress/integration/accomplishments.spec.js
+++ project-1/cypress/integration/accomplishments.spec.js
@@ -13,4 +13,15 @@ describe('Accomplishments dashboard', () => {
     cy.contains('Submit Accomplishment').click()
     cy.contains('Complete the items above to continue').should('be.visible')
   })
+
+  it('should show validation message', () => {
+    cy.getByTestId('accomplishment-title-input').type('Finished one of my apps')
+    cy.getByTestId('accomplishment-input').type('Learn code app')
+    cy.getByTestId('accomplishment-checkbox').type('Learn code app')
+
+    cy.contains('Submit Accomplishment').click()
+    cy.contains('This Accomplisment was Successfully Submitted').should(
+      'be.visible'
+    )
+  })
 })
