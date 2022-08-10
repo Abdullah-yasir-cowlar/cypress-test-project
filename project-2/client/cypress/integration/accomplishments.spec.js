97a18b53049336850df4b7197204964b1e50ebd6
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-08-10 20:14:07 +0500

    intercepted post request and sent mocked response in /accomplishment

--- project-2/client/cypress/integration/accomplishments.spec.js
+++ project-2/client/cypress/integration/accomplishments.spec.js
@@ -13,4 +13,18 @@ describe('Accomplishments dashboard', () => {
     cy.get('button').click()
     cy.contains('Your content is not appropriate').should('be.visible')
   })
+
+  it('should display inapp content error when text includes giraffe (with mock)', () => {
+    cy.intercept('POST', 'http://localhost:4000', req => {
+      req.reply(res => {
+        res.send({ msg: 'Your content is not appropriate' })
+      })
+    })
+
+    cy.get("[placeholder='Title'").type('This is my accomplishment')
+    cy.get("[placeholder='My accomplishment...']").type('I pet a giraffe')
+    cy.get("[type='checkbox']").check()
+    cy.get('button').click()
+    cy.contains('Your content is not appropriate').should('be.visible')
+  })
 })
