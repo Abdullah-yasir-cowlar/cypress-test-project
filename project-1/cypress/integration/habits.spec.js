0baac1c44f865c0773d02228137ae64af3e79320
abdullah-yasir-cowlar <abdullah.yasir.cowlar@gmail.com>
2022-08-10 15:16:44 +0500

    added test to check if icon changes when container el is clicked

--- project-1/cypress/integration/habits.spec.js
+++ project-1/cypress/integration/habits.spec.js
@@ -19,4 +19,14 @@ describe('habit dashboar', () => {
       .should('be.visible')
       .should('have.class', 'HabitCard__habit-container')
   })
+
+  it('should toggle icon when habit card is clicked', () => {
+    cy.get('#habit-add-btn').click()
+    cy.get('input[placeholder="Habit"]').type('Drink water in morning')
+    cy.contains('Save Changes').click()
+
+    cy.get('[src="/static/media/close.fa7e5ead.svg"]').should('be.visible')
+    cy.get('.HabitCard__habit-container').click()
+    cy.get('[src="/static/media/check.9e8832df.svg"]').should('be.visible')
+  })
 })
